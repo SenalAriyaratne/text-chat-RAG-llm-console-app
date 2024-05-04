5eb78f44c7cabcca356d497f07dec7680cce3ba4
Senal Ariyaratne <senalmari96@Senals-Mac-mini.local>
2024-05-04 22:03:24 +1000

    VectorDB Class update: finish initialize

--- vectordb.js
+++ vectordb.js
@@ -1,17 +1,42 @@
 import { OpenAIEmbeddings } from '@langchain/openai';
 import { Pinecone } from '@pinecone-database/pinecone';
-class VectorDataBase{
-    constructor(apikey){
+
+
+
+class VectorDataBase {
+    constructor(apikey) {
         this.apiKey = apikey;
+        this.index = null;
 
     }
 
-    initialize = async () => {
-        const vectordb = new Pinecone({
-            apiKey: apikey
-        });
+    initialize = async (indexname) => {
+        try {
+            const vectordb = new Pinecone({
+                apiKey: this.apiKey,
+            });
 
+            await vectordb.createIndex({
+                name: indexname,
+                dimension: 8,
+                metric: 'cosine',
+                spec: {
+                    serverless: {
+                        cloud: 'aws',
+                        region: 'us-east-1'
+                    }
+                }
+            });
+            
+            console.log(`Vector Database initialized with index name ${indexname}`)
+
+        } catch (error) {
+            console.log(`Error initializing PineCone DataBase  ${error.message}`);
+
+        }    
     }
 
 
 }
+
+export default VectorDataBase;
