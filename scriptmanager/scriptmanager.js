5220e03ab7d0500e5836fc4190ace8f8a05f4570
Senal Ariyaratne <senalmari96@Senals-Mac-mini.local>
2024-05-09 22:42:47 +1000

    new directory to hold ScriptManager class and scripts array

---
+++ scriptmanager/scriptmanager.js
@@ -0,0 +1,17 @@
+import scriptArray from "../promptscripts/scripts.js";
+
+class ScriptManager{
+
+    constructor(){
+        this.scriptBundle = scriptArray;
+    }
+
+    getScript = (agenttype) => {
+        const result = this.scriptBundle.find(item => item.agentType === agenttype);
+        return result ? result.script : null;
+    }
+
+
+}
+
+export default ScriptManager;
